8b53a4a1a819b94b3aaa6be4179a04fec0b502a6
Rama0 <ramiro.acosta.24@gmail.com>
2023-09-18 18:02:39 -0300

    Modificacion del parser
    Finalizacion de reglas faltan test

--- src/main/java/lyc/compiler/files/SymbolTableGenerator.java
+++ src/main/java/lyc/compiler/files/SymbolTableGenerator.java
@@ -7,6 +7,7 @@ public class SymbolTableGenerator implements FileGenerator{
 
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+        fileWriter.write("NOMBRE TIPODATO VALOR LONGITUD\n");
+        
     }
 }
